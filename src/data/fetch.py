716e499dacf3011e61b1d289e8b0f063972d0a3d
Adam S <adamsallie999@gmail.com>
2025-08-24 13:41:06 +0100

    New Repo Structure (based on improved main)

--- src/data/fetch.py
+++
@@ -1,8 +0,0 @@
-from .schemas import PriceDataSchema
-
-def get_price_data(ticker: str, start: str = "2015-01-01") -> pd.DataFrame:
-    import yfinance as yf
-    df = yf.download(ticker, start=start, auto_adjust=True)
-    df.index.name = "date"
-    PriceDataSchema.validate(df)
-    return df
